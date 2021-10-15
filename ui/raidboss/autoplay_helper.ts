b39dd87653c2a3284e4b37ee6222826fb7b904a4
Marc-Aurel Zent <marc_aurel@me.com>
2021-10-15 20:25:40 +0200

    making non-blink engines compatible with autoplay_helper

--- ui/raidboss/autoplay_helper.ts
+++ ui/raidboss/autoplay_helper.ts
@@ -1,21 +1,25 @@
 export default class AutoplayHelper {
-  static Check(): boolean {
+  static CheckChromium(): boolean {
     const context = new AudioContext();
     return context.state === 'suspended';
   }
 
   static Prompt(): void {
+    const context = new AudioContext();
     const button = document.createElement('button');
     button.innerText = 'Click to enable audio';
     button.classList.add('autoplay-helper-button');
     button.onclick = function() {
-      button.remove();
+        context.resume();
     };
+    context.onstatechange = function () {
+        button.remove();
+      }
     document.body.appendChild(button);
   }
 
   static CheckAndPrompt(): void {
-    if (AutoplayHelper.Check())
+    if (AutoplayHelper.CheckChromium())
       AutoplayHelper.Prompt();
   }
 }
